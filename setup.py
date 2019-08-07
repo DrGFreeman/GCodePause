26d9412e669037abe828c0d37cabe4fd0460db08
DrGFreeman <DrGFreeman@users.noreply.github.com>
2019-08-06 22:40:15 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from gcodepause import __version__
+
+setup(
+    name='gcodepause',
+    version=__version__,
+    description='A package to add pauses in 3D printing GCODE',
+    author='Julien de la Bruère-Terreault',
+    author_email='drgfreeman@tuta.io',
+    url='https://github.com/DrGFreeman/GCode-Layer-Pause',
+    license='MIT',
+    python_required='>=3.6',
+    install_requires=['pyyaml'],
+    py_modules=['gcodepause'],
+)
